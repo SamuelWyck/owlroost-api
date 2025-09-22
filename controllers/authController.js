c992b8830469f62e62135ea10c4cff4873a78540
samuel <samawyck@gmail.com>
2025-09-22 13:53:21 -0500

    Create handlers for users to log in, log out, and sign up

---
+++ controllers/authController.js
@@ -0,0 +1,86 @@
+const asyncHandler = require("express-async-handler");
+const db = require("../db/queries.js");
+const {validationResult} = require("express-validator");
+const {signupVal} = require("../utils/validators.js");
+const bcrypt = require("bcryptjs");
+const passport = require("../utils/passport.js");
+
+
+
+const signupPost = asyncHandler(async function(req, res, next) {
+    console.log(req.body)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const username = req.body.username;
+    const email = req.body.email;
+    const pwdHash = await bcrypt.hash(req.body.password, 10);
+
+    let user = null;
+    try {
+        user = await db.createUser(email, username, pwdHash);
+    } catch (error) {
+        return res.status(500).json(
+            {errors: [{msg: "Unable to create user"}]}
+        );
+    }
+    if (!user) {
+        return res.status(400).json(
+            {errors: [{msg: "Unable to create user"}]}
+        );
+    }
+
+    return next();
+});
+
+
+
+const loginPost = asyncHandler(async function(req, res) {
+    const authFunction = passport.authenticate("local", function(error, user, info) {
+        if (error) {
+            return res.status(500).json({
+                errors: [{msg: "Unable to log in user"}]
+            });
+        }
+        if (!user) {
+            return res.status(400).json({errors: [info]});
+        }
+
+        req.login(user, function(error) {
+            if (error) {
+                return res.status(500).json(
+                    {errors: [{msg: "Unable to log in user"}]}
+                );
+            }
+            return res.json({user});
+        });
+    });
+    authFunction(req, res);
+});
+
+
+
+function logoutPost(req, res) {
+    req.logout(function(error) {
+        if (error) {
+            return res.status(500).json({
+                errors: [{msg: "Unable to log out user"}]
+            });
+        }
+        return res.json({result: "success"});
+    });
+};
+
+
+
+module.exports = {
+    signupPost: [
+        signupVal,
+        signupPost,
+        loginPost
+    ],
+    loginPost,
+    logoutPost
+};
