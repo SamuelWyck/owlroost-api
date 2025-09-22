895d143f913dc30719e9183da1d6772bc3072311
samuel <samawyck@gmail.com>
2025-09-22 13:48:51 -0500

    Create route

---
+++ routes/authRoute.js
@@ -0,0 +1,16 @@
+const {Router} = require("express");
+const authController = require("../controllers/authController.js");
+const {isLoggedIn, isNotLoggedIn} = require("../utils/authMiddleware.js");
+
+
+
+const authRoute = Router();
+
+
+authRoute.post("/signup", isNotLoggedIn, authController.signupPost);
+authRoute.post("/login", isNotLoggedIn, authController.loginPost);
+authRoute.post("/logout", isLoggedIn, authController.logoutPost);
+
+
+
+module.exports = authRoute;
