27dd014dbf1af98706d01240bc134c3274463558
samuel <samawyck@gmail.com>
2025-09-23 16:37:46 -0500

    Create new post handler

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries.js");
+const {validationResult} = require("express-validator");
+const {newPostVal} = require("../utils/validators.js");
 
 
 
@@ -9,6 +11,40 @@ const postsGet = asyncHandler(async function(req, res) {
 
 
 
+const newPostPost = asyncHandler(async function(req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const userId = req.user.id;
+    const title = req.body.title;
+    const content = req.body.content;
+
+    let post = null;
+    try {
+        post = await db.createPost(userId, title, content);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(
+            {errors: [{msg: "Unable to create post"}]}
+        );
+    }
+    if (!post) {
+        return res.status(400).json(
+            {errors: [{msg: "Unable to create post"}]}
+        );
+    }
+
+    return res.json({result: "success"});
+});
+
+
+
 module.exports = {
-    postsGet
+    postsGet,
+    newPostPost: [
+        newPostVal,
+        newPostPost
+    ]
 };
