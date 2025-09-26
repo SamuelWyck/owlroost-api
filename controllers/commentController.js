4151f12bea67f64a7f589d88ace5f8af8a04a25a
samuel <samawyck@gmail.com>
2025-09-26 14:53:52 -0500

    Create handler for creating comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries.js");
+const {validationResult} = require("express-validator");
+const {newCommentVal} = require("../utils/validators.js");
 
 
 
@@ -33,11 +35,40 @@ const commentsGet = asyncHandler(async function(req, res) {
 
 
 const createCommentPost = asyncHandler(async function(req, res) {
-    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const userId = req.user.id;
+    const postId = req.body.postId;
+    const content = req.body.content;
+    let comment = null;
+    try {
+        comment = await db.createComment(
+            postId, userId, content
+        );
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(
+            {errors: [{msg: "Unable to create comment"}]}
+        );
+    }
+    if (!comment) {
+        return res.status(400).json(
+            {errors: [{msg: "Unable to create comment"}]}
+        );
+    }
+
+    return res.json({comment});
 });
 
 
 
 module.exports = {
-    commentsGet
+    commentsGet,
+    createCommentPost: [
+        newCommentVal,
+        createCommentPost
+    ]
 };
