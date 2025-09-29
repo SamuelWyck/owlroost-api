d91ace638fa16f321ca18126817d179e105506af
samuel <samawyck@gmail.com>
2025-09-29 09:59:25 -0500

    Add comment pagination

--- utils/paginationManager.js
+++ utils/paginationManager.js
@@ -1,9 +1,15 @@
 class PaginationManger {
     postTakeNum = 21;
+    commentTakeNum = 3;
 
 
     calcPostSkip(pageNum) {
         return (this.postTakeNum - 1) * pageNum;
+    };
+
+
+    calcCommentSkip(pageNum) {
+        return (this.commentTakeNum - 1) * pageNum;
     };
 };
 
