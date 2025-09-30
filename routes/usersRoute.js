5be849c0e66e944f4db1bd8ba888561c913f3c08
samuel <samawyck@gmail.com>
2025-09-30 18:19:16 -0500

    Create /:userId/follow and /:userId/unfollow routes

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,14 +1,17 @@
 const {Router} = require("express");
 const usersController = require("../controllers/usersController.js");
+const {isLoggedIn} = require("../utils/authMiddleware.js");
 
 
 
 const userRoute = Router();
 
 
-userRoute.get("/:userId/posts", usersController.userPostsGet);
-userRoute.get("/:userId", usersController.userProfileGet);
 userRoute.get("/", usersController.usersGet);
+userRoute.get("/:userId", usersController.userProfileGet);
+userRoute.get("/:userId/posts", usersController.userPostsGet);
+userRoute.post("/:userId/follow", isLoggedIn, usersController.followUserPost);
+userRoute.delete("/:userId/unfollow", isLoggedIn, usersController.unfollowUserDel);
 
 
 
