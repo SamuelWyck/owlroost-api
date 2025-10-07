e5a778871f8f8515d56b43473104d9cca50e209c
samuel <samawyck@gmail.com>
2025-10-07 09:55:50 -0500

    Add ca cert to pool

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,14 @@
 const {Pool} = require("pg");
 require("dotenv").config();
+const path = require("node:path");
+const fs = require("node:fs");
 
 
 
 module.exports = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        require: true,
+        ca: fs.readFileSync(path.join(__dirname, "../ca.pem")).toString()
+    }
 });
